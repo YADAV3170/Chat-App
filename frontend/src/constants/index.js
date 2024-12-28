e5d9e52e7141793ff4e5960e4dd06eab4bdeb180
unknown <ssy382415@gmail.com>
2024-12-28 20:34:13 +0530

    Porject completed

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,18 @@
+export const THEMES=[
+      "light",
+      "dark",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "sunset",
+      "dim",
+      "winter",
+      "coffee",
+      "night",
+      "dracula",
+      "mytheme",
+];
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,47 @@
+import daisyui from "daisyui";
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+  extend: {},
+  },
+  plugins: [daisyui],
+  daisyui:{
+    themes: [
+      "light",
+      "dark",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "sunset",
+      "dim",
+      "winter",
+      "coffee",
+      "night",
+      "dracula",
+      {
+        mytheme: {
+          "primary": "#d946ef",
+          "secondary": "#ff00ff",
+          "accent": "#f43f5e",
+          "neutral": "#ff00ff",
+          "base-100": "#f472b6",
+          "info": "#ffe4e6",
+          "success": "#00ff00",
+          "warning": "#fbbf24",         
+          "error": "#ff0000",
+                    },
+      }
+    ]
+  }
+  }
+
+  
